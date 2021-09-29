4390eb53868ec71529378fbf7f71ce0d8e01d3b7
aydevelop <juwric@gmail.com>
2021-09-29 09:37:22 +0300

    initial commit

---
+++ AvtoZapchasti/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AvtoZapchasti.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Content("HomeController Index");
+        }
+    }
+}
